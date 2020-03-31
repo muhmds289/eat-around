1fe3f3c65f6877b442c326e2b15c9f94838d1aa8
mohamed Ibrahim <muhmds@hotmail.com>
2020-03-31 19:36:57 -0400

    init

---
+++ public/js/restaurant.js
@@ -0,0 +1,1 @@
+console.log("restaurant.js loaded...")
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the  if it exists currently --
+DROP DATABASE IF EXISTS eat_around;
+-- Creates the "restuarant" database --
+CREATE DATABASE eat_around;
